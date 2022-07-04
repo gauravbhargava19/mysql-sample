3baee9ae6621b4239725785fb6e76b5bd0888836
gauravbhargava19 <gaurav.bhargava34@gmail.com>
2022-07-04 16:35:24 +0530

    Update test-script.sql

--- test-script.sql
+++ test-script.sql
@@ -1,9 +1,1 @@
-CREATE TABLE employees (
-   employee_id   NUMERIC      NOT NULL,
-   firstname    VARCHAR(255) NOT NULL,
-   lastname     VARCHAR(255) NOT NULL,
-   date_of_birth DATE                 ,
-   phone_number  VARCHAR(255) NOT NULL,
-   junk          CHAR(255)            ,
-   CONSTRAINT employees_pk PRIMARY KEY (employee_id)
-);
+
